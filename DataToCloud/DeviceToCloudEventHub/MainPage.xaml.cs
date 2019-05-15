7ae0b1578d137c6ec998ed556feecfeb3eaa3dd1
7Surfer <34557774+7Surfer@users.noreply.github.com>
2019-05-15 22:29:31 +0200

    moved sensor code into extra class

--- DataToCloud/DeviceToCloudEventHub/MainPage.xaml.cs
+++ DataToCloud/DeviceToCloudEventHub/MainPage.xaml.cs
@@ -42,8 +42,9 @@ namespace DeviceToCloudEventHub
 
         private static int timer_intervall_ms = 5000;
 
-        private I2cDevice i2cDevice;
+        
         private DispatcherTimer timer;
+        Si7021_sensor si7021_sensor;
 
         //Measuarment Class
         public class Measurenent
@@ -65,29 +66,19 @@ namespace DeviceToCloudEventHub
         {
             this.InitializeComponent();
 
+            si7021_sensor = new Si7021_sensor();
             //call async function at start of programm
             Async_start();
         }
-
-        //string deviceId = new EasClientDeviceInformation().FriendlyName;
 
         //need a async methode to start the task
         async void Async_start()
         {
             await Start();
         }
-
         private async Task Start()
         {
-            //return i2c controller
-            string i2cDeviceSelector = I2cDevice.GetDeviceSelector();
-            //find I2C bus controller device with selector string
-            IReadOnlyList<DeviceInformation> devices = await DeviceInformation.FindAllAsync(i2cDeviceSelector);
-
-            // create the settings and specify the device adress
-            // device adress from data sheet (https://www.silabs.com/documents/public/data-sheets/Si7021-A20.pdf Page 40)
-            var si7021_settings = new I2cConnectionSettings(0x40);
-            i2cDevice = await I2cDevice.FromIdAsync(devices[0].Id, si7021_settings);
+            si7021_sensor.Setup_device();
 
             // Start the polling timer.
             timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(timer_intervall_ms) };
@@ -97,36 +88,11 @@ namespace DeviceToCloudEventHub
 
         private void Timer_Tick(object sender, object e)
         {
-            // Read data from I2C.
-            var command = new byte[1];
-            var humidityData = new byte[2];
-            var temperatureData = new byte[2];
+            double temperature;
+            double humidity;
 
-            // Read humidity.
-            // adress got from data sheet (https://www.silabs.com/documents/public/data-sheets/Si7021-A20.pdf Page 40)
-            command[0] = 0xE5;
-            i2cDevice.WriteRead(command, humidityData);
-
-            // Read temperature.
-            command[0] = 0xE3;
-            i2cDevice.WriteRead(command, temperatureData);
-
-            // Calculate and report the humidity.
-            var rawHumidityReading = humidityData[0] << 8 | humidityData[1];
-            var humidityRatio = rawHumidityReading / (float)65536;
-            double humidity = -6 + (125 * humidityRatio);
-            //round to 2 decimal
-            humidity = Math.Round(humidity, 2);
-
-
-            // Calculate and report the temperature.
-            var rawTempReading = temperatureData[0] << 8 | temperatureData[1];
-            var tempRatio = rawTempReading / (float)65536;
-            double temperature = (-46.85 + (175.72 * tempRatio)) * 9 / 5 + 32;
-            //calculate from fahrenheit into Celcius
-            temperature = (temperature - 32) / 1.8; //(F - 32) ÷ 1.8
-            //round to 2 decimal
-            temperature = Math.Round(temperature, 2);
+            temperature = si7021_sensor.Get_temperature();
+            humidity = si7021_sensor.Get_humidity();
 
 
             //Call function to send data to cloud
@@ -138,7 +104,7 @@ namespace DeviceToCloudEventHub
         {
             string iotHubUri = "IoTHubHE.azure-devices.net";
             string deviceKey = "";
-            string deviceId2 = "yannik-rpi3";
+            string deviceId2 = "";
 
             var deviceClient = DeviceClient.Create(iotHubUri, AuthenticationMethodFactory.CreateAuthenticationWithRegistrySymmetricKey(deviceId2, deviceKey), TransportType.Http1);
 
---
+++ DataToCloud/DeviceToCloudEventHub/Si7021_sensor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Debug
+using System.Diagnostics;
+
+//I2C
+using Windows.Devices.I2c;
+using Windows.Devices.Enumeration;
+using Windows.UI.Core;
+using Microsoft.Azure.Devices.Client;
+
+namespace DeviceToCloudEventHub
+{
+    class Si7021_sensor
+    {
+        //Set the count of the rounded decimal number
+        private int accuracy = 2;
+
+        private I2cDevice i2cDevice;
+
+        //Setup device with i2c
+        public async void Setup_device()
+        {
+            //return i2c controller
+            string i2cDeviceSelector = I2cDevice.GetDeviceSelector();
+            //find I2C bus controller device with selector string
+            IReadOnlyList<DeviceInformation> devices = await DeviceInformation.FindAllAsync(i2cDeviceSelector);
+
+            // create the settings and specify the device adress
+            // device adress from data sheet (https://www.silabs.com/documents/public/data-sheets/Si7021-A20.pdf Page 40)
+            var si7021_settings = new I2cConnectionSettings(0x40);
+            i2cDevice = await I2cDevice.FromIdAsync(devices[0].Id, si7021_settings);
+        }
+
+        //Read Temperatuire from I2C
+        public double Get_temperature()
+        {
+            // Read data from I2C.
+            var command = new byte[1];
+            var temperatureData = new byte[2];
+
+
+            // adress got from data sheet (https://www.silabs.com/documents/public/data-sheets/Si7021-A20.pdf Page 40)
+            // Read temperature.
+            command[0] = 0xE3;
+            i2cDevice.WriteRead(command, temperatureData);
+
+            // Calculate and report the temperature.
+            var rawTempReading = temperatureData[0] << 8 | temperatureData[1];
+            var tempRatio = rawTempReading / (float)65536;
+            double temperature = (-46.85 + (175.72 * tempRatio)) * 9 / 5 + 32;
+            //calculate from fahrenheit into Celcius
+            temperature = (temperature - 32) / 1.8; //(F - 32) ÷ 1.8
+            //round to 2 decimal
+            temperature = Math.Round(temperature, accuracy);
+
+            return temperature;
+        }
+
+        //Read Humidity from I2C
+        public double Get_humidity()
+        {
+            // Read data from I2C.
+            var command = new byte[1];
+            var humidityData = new byte[2];
+
+            // Read humidity.
+            // adress got from data sheet (https://www.silabs.com/documents/public/data-sheets/Si7021-A20.pdf Page 40)
+            command[0] = 0xE5;
+            i2cDevice.WriteRead(command, humidityData);
+
+            // Calculate and report the humidity.
+            var rawHumidityReading = humidityData[0] << 8 | humidityData[1];
+            var humidityRatio = rawHumidityReading / (float)65536;
+            double humidity = -6 + (125 * humidityRatio);
+            //round to 2 decimal
+            humidity = Math.Round(humidity, accuracy);
+
+            return humidity;
+        }
+    }
+}
