ebfc4a213684d5f07eec528de44ffe6daf3393e3
crorson <49120000+crorson@users.noreply.github.com>
2019-05-27 14:58:00 +0200

    List added

--- component-test(2405)/src/app/warning/warning.component.ts
+++ component-test(2405)/src/app/warning/warning.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
+export interface PeriodicElement {
+  roomName: string;
+  warningText: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {roomName: 'Raum 1', warningText: 'Fehlertext'},
+  {roomName: 'Raum 2', warningText: 'Fehlertext'}
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
 @Component({
   selector: 'app-warning',
   templateUrl: './warning.component.html',
   styleUrls: ['./warning.component.css']
 })
-export class WarningComponent implements OnInit {
+  
+export class WarningComponent {
+  displayedColumns: string[] = ['roomName', 'warningText'];
+  dataSource = ELEMENT_DATA;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  data = [
+    {id: 'Raum1', description: 'Test1', },
+    {id: 'Raum2', description: 'Test2', },
+    {id: 'Raum3', description: 'Test3', },
+    {id: 'Raum4', description: 'Test4', },
+    {id: 'Raum5', description: 'Test5', },
+    {id: 'Raum6', description: 'Test6', },
+    {id: 'Raum7', description: 'Test7', },
+    {id: 'Raum8', description: 'Test8', },
+    {id: 'Raum9', description: 'Test9', },
+  ]
 }
