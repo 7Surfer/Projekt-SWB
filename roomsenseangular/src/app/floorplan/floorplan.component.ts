a1a07183b433d6e210880350456a0dc02bb86ffb
crorson <49120000+crorson@users.noreply.github.com>
2019-06-03 17:46:53 +0200

    Linked Create-Room to Main-Page

--- roomsenseangular/src/app/floorplan/floorplan.component.ts
+++ roomsenseangular/src/app/floorplan/floorplan.component.ts
@@ -8,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class FloorplanComponent implements OnInit {
 
   constructor() { }
+  
+  data = [
+    {id: 'Raum1', temperature: "2°C", humidity: "40%"},
+    {id: 'Raum2', temperature: "1°C", humidity: "40%"},
+    {id: 'Raum3', temperature: "2°C", humidity: "50%"},
+    {id: 'Raum4', temperature: "5°C", humidity: "40%"},
+    {id: 'Raum5', temperature: "2°C", humidity: "60%"},
+    {id: 'Raum6', description: 'Test6', },
+    {id: 'Raum7', description: 'Test7', },
+    {id: 'Raum8', description: 'Test8', },
+    {id: 'Raum9', description: 'Test9', },
+  ]
 
   ngOnInit() {
   }
 
 }
+
